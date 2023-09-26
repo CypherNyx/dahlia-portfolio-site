27c12c70ba0925e94ecfede48f5d4987d00ee24f
Dahlia Guido <dguido.dev@gmail.com>
2023-09-26 11:23:11 -0500

    adding social placeholders to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { Link } from "react-scroll";
+import { BorderClear } from "@mui/icons-material";
 
 
 const Footer = () => {
@@ -42,13 +43,13 @@ const Footer = () => {
           Projects
         </Link>
         
-        <Link 
+        {/* <Link 
           to="about" 
           className="link" 
           smooth 
           style={{ color: "white", margin: "0.5rem 0", cursor: "pointer" }}>
           About
-        </Link>
+        </Link> */}
         
         <Link 
           to="resume" 
@@ -56,8 +57,39 @@ const Footer = () => {
           smooth 
           style={{ color: "white", margin: "0.5rem 0", cursor: "pointer" }}>
           Resume
-        </Link>
-        
+        </Link>        
+      </Box>
+
+      <Box sx={{ display: "flex", flexDirection: "column"}}>
+      <Typography 
+          variant="body" 
+          sx={{ color: "#9400FF", fontWeight: "bold", mb:2 }}
+        >
+          SOCIAL
+        </Typography>
+
+        <Box sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}>
+          <GitHubIcon sx={{ mr: 2, color: "white" }} />
+          <Typography variant="body2" sx={{ color: "white", my: 1 }}>
+            GitHub
+          </Typography>
+        </Box>
+
+        <Box sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}>
+          <EmailIcon sx={{ mr: 2, color: "white" }} />
+          <Typography variant="body2" sx={{ color: "white", my: 1 }}>
+            Email
+          </Typography>
+        </Box>
+
+        <Box sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}>
+          <LinkedInIcon sx={{ mr: 2, color: "white" }} />
+          <Typography variant="body2" sx={{ color: "white", my: 1 }}>
+            LinkedIn
+          </Typography>
+        </Box>
+
+
       </Box>
     </Box>
   );
