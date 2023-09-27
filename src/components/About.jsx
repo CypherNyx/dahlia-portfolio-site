d32dfa1f9173e84d402c437512a51a70d636ea92
Dahlia Guido <dguido.dev@gmail.com>
2023-09-27 15:07:31 -0500

    adding the about me section and avatar placeholder for profile image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
-import { Box, Button, Typography, styled } from "@mui/material";
+import { Avatar, Box, Button, Typography, styled } from "@mui/material";
 import React from "react";
 import Testimonial from "./Testimonial";
+import dahliaAvatar from "../media/dahliaAvatar.png";
 
 
 
@@ -45,11 +46,98 @@ const About = () => {
     },
   }));
 
+  const AvatarBox = styled(Box)(({ theme }) => ({
+    maxWidth: "1300px",
+    display: "flex", 
+    justifyContent: "space-between", 
+    mx: "auto", 
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column"
+    }
+  }));
+
+  
+  const TextBox = styled(Box)(({ theme }) => ({
+    marginLeft: "1.5rem",
+    flex: 1,
+    display: "flex", 
+    justifyContent: "space-around", 
+    [theme.breakpoints.down("md")]: {
+      marginLeft: "0",
+      marginTop: "3rem",
+    },
+  }));
+
   
   
   
   return (
     <Box sx={{ maxWidth: "1300px", my: 10, padding: 3, mx: "auto" }} id="about">
+      <CustomTitleBox
+        sx={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <Typography
+          variant="h4"
+          style={{ color: "white", textAlign: "center" }}
+        >
+          Howdy! I'm Dahlia,
+        </Typography>
+        <div
+          style={{
+            height: "5px",
+            backgroundColor: "#00C7FF",
+            width: "60%",
+            border: 0,
+            borderRadius: "25px",
+          }}
+        ></div>
+      </CustomTitleBox>
+
+      <AvatarBox sx={{ my: 7, padding: 2 }}>
+      <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", flex: 1 }}>
+      <Avatar
+          alt="Dahlia's profile picture"
+          src={dahliaAvatar}
+          sx={{ width: 400, height: 400 }}
+        />
+      </Box>
+      <TextBox>
+      <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <Typography variant="body1" color="white" sx={{ maxWidth: 575, margin: "1rem"}}>
+          I am a full-stack developer driven to build fast, responsive websites with appealing and intuitive interfaces. Having recently graduated from the University of Texas at Austin's Full-Stack Coding Bootcamp, I'm eager to grow my skills in a role focused on continuous learning and development.
+          </Typography>
+
+          <Typography variant="body1" color="white" sx={{ maxWidth: 575, margin: "1rem"}}>
+          With a blend of creative and analytical strengths, I'm passionate about crafting user-friendly digital experiences. I believe deeply in the web's power to connect, engage, and promote accessibility.
+          </Typography>
+
+          <Typography variant="body1" color="white" sx={{ maxWidth: 575, margin: "1rem"}}>
+          Please explore my portfolio - I'd love to discuss how we can work together to build products that are both aesthetically and functionally exceptional. As a developer dedicated to constant improvement, I'm excited to push my abilities further and deliver solutions that positively impact users. Let's collaborate to make the web more innovative, connected, and inclusive.
+          </Typography>
+          </Box>
+      </TextBox>
+
+        
+      </AvatarBox>
+
+      <div
+        style={{
+          height: "5px",
+          backgroundColor: "#00C7FF",
+          width: "100%",
+          mx: "auto",
+          border: 0,
+          borderRadius: "25px",
+        }}
+      ></div>
+
+      
+      {/*** Future Section for Formal Endorsements: **** 
       <CustomTitleBox
         sx={{
           width: "100%",
@@ -91,6 +179,8 @@ const About = () => {
           borderRadius: "25px",
         }}
       ></div>
+ */}
+
 
       <Typography
         variant="h4"
